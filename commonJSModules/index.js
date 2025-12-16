bb19b5e4665255a6bb23918b703818880b8a9e3d
Abhinav <AbhinavSunil@hotmail.com>
2025-12-16 22:03:03 +0530

    commonJSModules

---
+++ commonJSModules/index.js
@@ -0,0 +1,2 @@
+const { linearSearch } = require('./searching'); // Destructure the function
+console.log(linearSearch([10, 23, 45, 70, 11, 15], 70));
---
+++ commonJSModules/searching.js
@@ -0,0 +1,27 @@
+const linearSearch = function (arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == target) {
+      return i;
+    }
+  }
+  return undefined;
+};
+const binarySearch = function (arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] == target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return undefined;
+};
+module.exports = {
+  linearSearch,
+  binarySearch,
+};
