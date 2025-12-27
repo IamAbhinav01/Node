855b3b300a8d7c2c1d7b7291b31bd3c91d0fe5c1
Abhinav <AbhinavSunil@hotmail.com>
2025-12-27 18:57:37 +0530

    learned about configuration

--- todoApisetup/src/index.js
+++ todoApisetup/src/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
+const { PORT } = require('./config/server.config');
 const app = express();
-const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`the server is running on port ${PORT}`);
 });
