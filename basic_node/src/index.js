226f7e3dbb6e3c33a7fadbe22cffb09537a5fb55
Abhinav <AbhinavSunil@hotmail.com>
2026-05-04 21:53:30 +0530

    Feat : added loggers(winston loggers)

--- basic_node/src/index.js
+++ basic_node/src/index.js
@@ -1,4 +1,5 @@
-const ServerConfig = require('./config/index');
+const { ServerConfig } = require('./config/');
+const { Logger } = require('./config/');
 const express = require('express');
 const apiRoutes = require('./routes');
 const app = express();
@@ -7,4 +8,5 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`successfully started server at port ${ServerConfig.PORT}`);
+  Logger.info('successfully started node express application');
 });
